de63bac70b7f040577637c7f2de956a447de5378
msuarz <mikemps@gmail.com>
2009-09-18 14:29:47 +0000

    signed the assembly & added a few should assertions

--- FluentSpec/ShouldAssertions.cs
+++ FluentSpec/ShouldAssertions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FluentSpec {
@@ -34,13 +35,31 @@ namespace FluentSpec {
             if (!Failed) throw new Exception("Should have failed");
         }
         
+        public static void ShouldFail<T>(this object Obj, Func<T> Func) {
+            var Failed = false;
+            
+            try { Func.Invoke(); }
+            catch { Failed = true; }
+            
+            if (!Failed) throw new Exception("Should have failed");
+        }
+        
         public static void ShouldNotFail(this object Obj, Action Action) {
             try { Action.Invoke(); }
             catch { throw new Exception("Should not have failed"); }
         }
         
+        public static void ShouldNotFail<T>(this object Obj, Func<T> Func) {
+            try { Func.Invoke(); }
+            catch { throw new Exception("Should not have failed"); }
+        }
+        
         public static void ShouldBeTrue(this bool Condition) {
             Assert.IsTrue(Condition);
+        }
+        
+        public static void ShouldBeTrue<T>(this T Condition) {
+            Assert.IsTrue(Convert.ToBoolean(Condition));
         }
         
         public static void ShouldBeFalse(this bool Condition) {
@@ -49,6 +68,22 @@ namespace FluentSpec {
         
         public static void ShouldHaveFailed(this object Obj) {
             Assert.Fail("Should have failed");
+        }
+        
+        public static void ShouldBeEmpty<T>(this List<T> List) {
+            Assert.IsTrue(List.Count == 0, "List have " + List.Count + " item(s)");
+        }
+
+        public static void ShouldContain<T>(this List<T> List, T Item) {
+            Assert.IsTrue(List.Contains(Item), "List does not have " + Item);
+        }
+        
+        public static void ShouldNotBeEmpty(this string String) {
+            Assert.IsTrue(String.IsNullOrEmpty(String), "'" + String + "' is not empty");
+        }
+        
+        public static void ShouldBeGreaterThan(this int Higher, int Lower) {
+            Assert.IsTrue(Higher > Lower, Higher + " is < than " + Lower);
         }
     }
 }
